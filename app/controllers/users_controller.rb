a66bba1dfea9d7734b4a0073cc58b984e3c94300
jimmy.huangjin <jimmy.huangjin@gmail.com>
2013-07-25 14:36:31 +0800

    users show page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+# coding: utf-8
+class UsersController < ApplicationController
+  def show
+      
+  end  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 WishList::Application.routes.draw do
   root "welcome#index"
   resources :wishes
+  resources :users
 end
